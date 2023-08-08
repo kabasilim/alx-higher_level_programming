0655acbcf4ce86b2202de3821d807a0e2a78b9da
Abasilim Kosisochukwu Obaejeogu <obaejeoguabasilim@gmail.com>
2023-08-08 15:48:12 -0700

    lists.h

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,14 +1,15 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
- */
+ * 
+*/
 typedef struct listint_s
 {
 	int n;
