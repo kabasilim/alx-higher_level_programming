17ef82647a3e8062a417b39ea8c0143da47b3f15
Abasilim Ksosichukwu <obaejeoguabasilim@gmail.com>
2023-08-30 15:05:52 +0000

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/103-tests.c
@@ -0,0 +1,14 @@
+/**
+ * print_pyhton_list - function that prints list
+ * 
+ */
+
+
+void print_python_list(PyObject *P)
+{
+	if(!PyList_Check(p))
+	{
+		printf("Error: p is not a valid PyListObject\n");
+	return;
+	}
+}
