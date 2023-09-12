096b7f91342f0056acf81c93939365ba213ad83f
Abasilim Ksosichukwu <obaejeoguabasilim@gmail.com>
2023-09-12 00:23:57 +0000

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""The Square class derived from the BaseGeometry class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """The Square Class"""
+    def __init__(self, size):
+        """initialization"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """The str method"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
